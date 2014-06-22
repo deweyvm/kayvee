21dd83eba9b92fb3e28667a21c19491423d78dcd
deweyvm <deweyvm@gmail.com>
2014-06-22 10:59:03 -0400

    working insertion

--- src/Database/Kayvee/Kayvee.hs
+++ src/Database/Kayvee/Kayvee.hs
@@ -2,17 +2,23 @@
 module Database.Kayvee.Kayvee where
 
 import Control.Applicative hiding (empty)
+import Control.Exception
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Internal as BS (c2w, w2c)
+import Data.Bits
 import System.Posix.Types
 import System.Posix.Files
 import System.Directory
+import System.IO
+import System.IO.Unsafe
 import Data.Hash.MD5
 import Data.Binary.Get
 import Data.Binary.Put
 import Data.Word
 import Data.List (genericLength)
 import Data.ByteString.Char8 (pack)
-
+import Debug.Trace
 type Hash = (Word32, Word32, Word32, Word32)
 
 data Error = Error String
@@ -35,7 +41,6 @@ schemaPath = "db.schema"
 dbPath :: FilePath
 dbPath = "db.store"
 
-
 getOffsets :: Hash -> IO [Pointer]
 getOffsets hash = do
     h <- BL.readFile schemaPath
@@ -44,9 +49,6 @@ getOffsets hash = do
 tup4 :: (Maybe a, Maybe b, Maybe c, Maybe d) -> Maybe (a, b, c, d)
 tup4 (Just w, Just x, Just y, Just z) = Just (w, x, y, z)
 tup4 _ = Nothing
-
-tup2 :: (Maybe a, Maybe b) -> Maybe (a, b)
-tup2 = uncurry (liftA2 (,))
 
 findOffsets :: Hash -> Get [Pointer]
 findOffsets hash = do
@@ -62,7 +64,6 @@ findOffsets hash = do
                                  else findOffsets hash
         _ -> return []
 
-
 readWord :: Get a -> Get (Maybe a)
 readWord get' = do
     empty <- isEmpty
@@ -71,24 +72,20 @@ readWord get' = do
     else do word <- get'
             return $ Just word
 
-
 createIfNotExists :: FilePath -> IO ()
 createIfNotExists fp = do
     exists <- doesFileExist fp
-    if not exists
+    if not exists || True
     then writeFile fp ""
     else return ()
 
 connect :: IO ()
 connect = do
-
     createIfNotExists dbPath
     createIfNotExists schemaPath
 
-
 disconnect :: IO ()
 disconnect = return ()
-
 
 getSize :: FilePath -> IO FileOffset
 getSize path =
@@ -109,8 +106,6 @@ hashPut s p = do
     putWord32be h3
     putWord64be p
 
-
-
 rawPut :: String -> String -> Put
 rawPut s v = do
     let ss = toPointer $ (genericLength s)
@@ -122,8 +117,65 @@ rawPut s v = do
     let pv = pack v
     putByteString pv
 
+readAt :: FilePath
+       -> Pointer --position
+       -> Pointer --size of data to read
+       -> IO BL.ByteString
+readAt fp pos size = do
+    h <- openBinaryFile fp ReadMode
+    hSeek h AbsoluteSeek (toInteger pos)
+    chars <- getChars h (toInteger size)
 
+    return $ (traceShow chars) $ BL.pack (BS.c2w <$> chars)
 
+bsToSize :: BL.ByteString -> Word64
+bsToSize bs =
+    case (fromInteger . toInteger) <$> BL.unpack bs of
+        (b0:b1:b2:b3:b4:b5:b6:b7:[]) ->
+            shiftL b0 56
+                .|. shiftL b1 48
+                .|. shiftL b2 40
+                .|. shiftL b3 32
+                .|. shiftL b4 24
+                .|. shiftL b5 16
+                .|. shiftL b6 8
+                .|. shiftL b7 0
+        xs -> error $ "malformed size " ++ show bs ++ " of length " ++ show (length xs)
+
+readValue :: FilePath -> Pointer -> IO BL.ByteString
+readValue fp ptr = do
+    size <- bsToSize <$> readAt fp ptr 8
+    readAt fp (ptr + 8) size
+
+getChars :: Handle -> Integer -> IO [Char]
+getChars h i = do
+    helper i
+    where helper i = if i == 0
+                     then return []
+                     else do c <- hGetChar h
+                             rest <- helper (i - 1)
+                             return $ c : rest
+
+toString :: BL.ByteString -> String
+toString x = BS.w2c <$> BL.unpack x
+
+findKey :: FilePath -> String -> [Pointer] -> IO (Maybe Pointer)
+findKey fp s (x:xs) = do
+    size <- bsToSize <$> readAt fp x 8
+    bs <- readAt fp (x + 8) size
+    let string = toString bs
+    if string == s
+    then return $ Just (x + 8 + size)
+    else findKey fp s xs
+findKey _ _ [] = return Nothing
 
 get :: String -> IO (Maybe String)
-get = undefined
+get key = do
+    let hash = makeHash key
+    offsets <- getOffsets hash
+    key <- findKey dbPath key offsets
+    case key of
+        Just k -> do x <- readValue dbPath k
+                     return $ Just $ toString x
+        Nothing -> return Nothing
+
--- src/Main.hs
+++ src/Main.hs
@@ -4,5 +4,7 @@ import Database.Kayvee.Kayvee
 main :: IO ()
 main = do
     connect
-    put "this" "is a test"
+    put "this is the key to the first element" "this is the contents of the first insertion"
+    x <- get "this is the key to the first element"
+    print x
     disconnect
